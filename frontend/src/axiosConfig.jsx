1be2b49cc022d72ba696e9e3a5909713f3e13938
arosh <aroshvitharana1234@gmail.comgit config --global user.name aroshgit config --global user.email aroshvitharana1234@gmail.com>
2025-08-07 14:32:38 +1000

    change of BAse URI

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.210.246.220/:5001', // Server
+  baseURL: 'http://3.25.117.215/:5001', // Server
 
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
